d98581dd3001306c0628567b1012ed3e97626621
xXATRIXx <22373295@buaa.edu.cn>
2026-05-09 00:19:51 +0800

    tem

--- FlipFlop_original/tianshu/exp/softmax_benchmark.py
+++ FlipFlop_original/tianshu/exp/softmax_benchmark.py
@@ -73,9 +73,9 @@ class OpTest(BaseOperatorTest):
     def torch_operator(self, *args, **kwargs):
         return torch.nn.functional.softplus(*args, **kwargs)
 
-    # def infinicore_operator(self, *args, **kwargs):
-    #     """InfiniCore implementation (operator not yet available)."""
-    #     return infinicore.nn.functional.softplus(*args, **kwargs)
+    def infinicore_operator(self, *args, **kwargs):
+        """InfiniCore implementation (operator not yet available)."""
+        return infinicore.nn.functional.softplus(*args, **kwargs)
 
 
 def main():
