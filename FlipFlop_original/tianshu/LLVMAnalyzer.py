1a8213f6240a60317bab5076439d96e20f67f584
xXATRIXx <22373295@buaa.edu.cn>
2026-05-17 03:30:00 +0800

    tem

--- FlipFlop_original/tianshu/LLVMAnalyzer.py
+++ FlipFlop_original/tianshu/LLVMAnalyzer.py
@@ -94,8 +94,8 @@ class LLVMAnalyzer:
         for b_idx in range(len(self.basic_blocks)):
             # 这里提前构建寄存器图
             self._count_block_insts(b_idx)
-        for str1,r in self.reg_map:
-            print(f"reg is:{str1} {r}")
+        for reg_name, inst_info in self.reg_map.items():
+            print(f"reg is: {reg_name} -> {inst_info}")
 
         self._analyze_branch_conditions()
         self._analyze_launch_factor()
